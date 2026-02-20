c004710c16a6d34dfb810f6cfc1bd1340801f88b
sivamanikanta18 <sivamanikanta92005@gmail.com>
2026-02-20 22:27:32 +0530

    changed

--- frontend/src/components/MinimalLayout.jsx
+++ frontend/src/components/MinimalLayout.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./MinimalLayout.css";
 
 const MinimalLayout = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <div className="minimal-layout">
+      <div className="minimal-layout-header">
+        <Header />
+      </div>
+      <div className="minimal-layout-content">
+        <Outlet />
+      </div>
+      <div className="minimal-layout-footer">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
